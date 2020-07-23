d297f23b4d30ab65182480a8143f0d7f78145214
Mattooz <nick.frame76@outlook.it>
2020-07-23 02:00:06 +0200

    Switched normal pointers to shared and weak pointers inside of map class. Added and unit tested map factory class.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,5 @@
-#include "map_factory.h"
-#include <iostream>
-
-using namespace std;
-using namespace quarantine_game;
-
 int main() {
-    quarantine_game::map map = map_factory::from_name("default-map");
 
-    cout << map["Ponticello"].lock()->_cost() << endl;
 }
 
 //TODO add functions
